177bb5a0f3ead1767860959e22a404277c317941
Aditya Vardhan Sharma <adityaradhika13@gmail.com>
2025-02-12 17:44:18 +0530

    Update page.jsx

--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,5 +1,29 @@
-import { SignUp } from '@clerk/nextjs'
+import {SignIn, SignUp} from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignUp />
+  return (
+      <section className="bg-gradient-to-b from-blue-600 to-blue-800 min-h-screen flex items-center justify-center">
+        <div className="w-full max-w-md bg-gradient-to-b from-blue-400 to-blue-600 rounded-lg shadow-2xl p-8">
+          <div className="text-center mb-8">
+            <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+              Welcome to Talent Trail
+            </h1>
+
+          </div>
+          <SignUp
+              appearance={{
+                  elements: {
+                      rootBox: "bg-gray-50 shadow-md rounded-lg", // Style the main container
+                      card: "p-6 bg-white rounded-md",          // Style the card container
+                      logoBox: "hidden",
+                      headerTitle: "text-2xl font-bold text-gray-800", // Style the title
+                      formFieldInput: "border-gray-300 focus:ring-blue-500 focus:border-blue-500", // Input fields
+                      formButtonPrimary: "bg-blue-600 hover:bg-blue-700 text-white font-semibold", // Buttons
+                  },
+              }}/>
+        </div>
+      </section>
+  );
+
+
 }
